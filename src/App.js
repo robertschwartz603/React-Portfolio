5bbd6eb4647111903c5e29891f08e702eff37585
Bobby Schwartz <robertschwartz603@gmail.com>
2022-05-17 21:50:45 -0400

    imported roboto and MUI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import HeaderDiv from "./components/Header";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return <HeaderDiv />;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,65 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const name = 'Christian';
-const thoughts = 'is amazing!!!';
+// const name = 'Christian';
+// const thoughts = 'is amazing!!!';
+
+{/* <h1>Hi! My name is ({name})</h1>
+<h2>My name has ({name.length}) letters</h2>
+<h2>I think React ({thoughts})</h2> */}
 
 function HeaderDiv() {
   return (
-    <div className="main-container">
-      <div className="container">
-        <div className="jumbotron">
-          <h1>Hi! My name is ({name})</h1>
-          <h2>My name has ({name.length}) letters</h2>
-          <h2>I think React ({thoughts})</h2>
-        </div>
-      </div>
-    </div>
+    <Box sx={{ width: '100%', maxWidth: 500 }}>
+      <Typography variant="h1" component="div" gutterBottom>
+        h1. Heading
+      </Typography>
+      <Typography variant="h2" gutterBottom component="div">
+        h2. Heading
+      </Typography>
+      <Typography variant="h3" gutterBottom component="div">
+        h3. Heading
+      </Typography>
+      <Typography variant="h4" gutterBottom component="div">
+        h4. Heading
+      </Typography>
+      <Typography variant="h5" gutterBottom component="div">
+        h5. Heading
+      </Typography>
+      <Typography variant="h6" gutterBottom component="div">
+        h6. Heading
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom component="div">
+        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom component="div">
+        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+        quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+        quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="button" display="block" gutterBottom>
+        button text
+      </Typography>
+      <Typography variant="caption" display="block" gutterBottom>
+        caption text
+      </Typography>
+      <Typography variant="overline" display="block" gutterBottom>
+        overline text
+      </Typography>
+    </Box>
   );
 }
 
