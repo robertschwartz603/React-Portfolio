edb22524eb7528df2695f86821891fccb4dd77c0
Bobby Schwartz <robertschwartz603@gmail.com>
2022-05-23 19:27:47 -0400

    hashrouter fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 import Header from './components/Header';
